fdbbbb7d5ac8e7773165f69add33295c787107a3
craigpeters@me.com <craigpeters@me.com>
2026-02-15 16:47:09 +0000

    Accessory Widget

--- Widgets/Widget/DueWidget.swift
+++ Widgets/Widget/DueWidget.swift
@@ -25,7 +25,7 @@ struct DueWidget: Widget {
         }
         .configurationDisplayName("Clarity Tasks")
         .description("View your tasks and weekly progress")
-        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge, .accessoryRectangular, .accessoryCircular])
     }
 }
 
@@ -35,45 +35,67 @@ struct ClarityTaskWidgetView: View {
     let gradient = Gradient(colors: [ .red, .yellow, .orange, .green])
     
     var body: some View {
-        
-        VStack(alignment: .leading) {
-            HStack {
-                HStack {
-                    Image("clarity-transparent")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 40, alignment: .init(horizontal: .leading, vertical: .center))
-                    Text(entry.filter.localizedStringResource)
-                        .font(.title3)
-                }
-                .fixedSize()
-                .frame(alignment: .leading)
-            }
-            
-            Spacer()
-            if widgetFamily  == .systemSmall {
-                // Just show tasks total
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("\(entry.todos.count)")
-                        .font(.system(size: 42, weight: .bold, design: .rounded))
-                        .foregroundStyle(.primary)
-                }
-            } else {
-                ForEach(entry.todos.prefix(widgetFamily == .systemMedium ? 3 : 10), id: \.id ) { todo in
-                    TaskRowInteractive(task: todo)
-                }
-            }
-            Spacer()
-            HStack {
+        if widgetFamily == .accessoryCircular {
+            Gauge(value: Double(entry.progress.completed), in: 0.0...Double(entry.progress.target)) {
                 Image(systemName: "target")
-                    .foregroundStyle(.orange)
-                Gauge(value: ((Double(entry.progress.completed) / Double(entry.progress.target)))) {
+            } currentValueLabel: {
+                Text(String(entry.todos.count))
+            } minimumValueLabel: {
+                Text(String(0))
+            } maximumValueLabel: {
+                Text(String(entry.progress.target))
+            }
+            .gaugeStyle(.accessoryCircular)
+        } else {
+            VStack(alignment: .leading) {
+                HStack {
+                    if widgetFamily == .accessoryRectangular {
+                        
+                    } else {
+                        HStack {
+                            Image("clarity-small")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, alignment: .init(horizontal: .leading, vertical: .center))
+                            if widgetFamily != .systemSmall {
+                                Text(entry.filter.localizedStringResource)
+                                    .font(.title3)
+                            }
+                        }
+                        .fixedSize()
+                        .frame(alignment: .leading)
+                    }
                 }
-                .gaugeStyle(LinearCapacityGaugeStyle())
-                .tint(gradient)
+                
+                Spacer()
+                if widgetFamily  == .systemSmall || widgetFamily == .accessoryRectangular {
+                    // Just show tasks total
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                            Text("\(entry.filter.localizedStringResource) Due: \(entry.todos.count)")
+                                .font(.headline)
+                                .foregroundStyle(.primary)
+                        
+
+                    }
+                } else {
+                    ForEach(entry.todos.prefix(widgetFamily == .systemMedium ? 3 : 10), id: \.id ) { todo in
+                        TaskRowInteractive(task: todo)
+                    }
+                }
+                Spacer()
+                HStack {
+                    Image(systemName: "target")
+                        .foregroundStyle(.orange)
+                    Gauge(value: ((Double(entry.progress.completed) / Double(entry.progress.target)))) {
+                    }
+                    .gaugeStyle(LinearCapacityGaugeStyle())
+                    .tint(gradient)
+                }
             }
         }
+        
+
     }
 }
 
@@ -91,6 +113,18 @@ struct ClarityTaskWidgetView: View {
 }
 
 #Preview("Large", as: .systemLarge) {
+    DueWidget()
+} timeline: {
+    PreviewData.shared.getPreviewTaskWidgetEntry()
+}
+
+#Preview("Accessory - Rectangular", as: .accessoryRectangular) {
+    DueWidget()
+} timeline: {
+    PreviewData.shared.getPreviewTaskWidgetEntry()
+}
+
+#Preview("Accessory - Circular", as: .accessoryCircular) {
     DueWidget()
 } timeline: {
     PreviewData.shared.getPreviewTaskWidgetEntry()
